f77671b4db5de65e52fa4ca64ad10c3e26f46745
ismael-miguel <espertalhao04@hotmail.com>
2015-02-26 10:32:22 +0000

    Update mariogolf.js

--- js/mariogolf.js
+++ js/mariogolf.js
@@ -4,11 +4,11 @@
 	
 	window.MarioGolf = function( code ){
 		var tmp = [];
-		if( !code || !code.match( /(?:^|[\r\n])([MLPBTYK]+)\|(?:(.*)\|)?([OQ|\-1RNM])(?:$|[\r\n])/ ) )
+		if( !code || !( code = code.match( /(?:^|[\r\n])([MLPBTYK]+)\|(?:(.*)\|)?([OQ|\-1RNM])(?:$|[\r\n])/ ) ) )
 		{
 			throw new SyntaxError( 'Expecting identifier, got ' + (code? 'invalid program' : '0-length code' ) + '.', 'code', 0 );
 		}
-		else if( ( tmp[0] = code[1].matches( /\{(?=([^"]*"[^"]*")*[^"]*$)/g ) ) != ( tmp[1] = code[1].matches( /\}(?=([^"]*"[^"]*")*[^"]*$)/g ) ) )
+		else if( code[2] != undefined && ( tmp[0] = code[2].match( /\{(?=([^"]*"[^"]*")*[^"]*$)/g ) || [] ).length != ( tmp[1] = code[2].match( /\}(?=([^"]*"[^"]*")*[^"]*$)/g ) || [] ).length )
 		{
 			throw new SyntaxError( 'Missing "' + ( tmp[0] < tmp[1] ? '{' : '}' ) + '" before the last identifier.', 'code', 0 );
 		}
@@ -18,7 +18,7 @@
 			return:'',
 			input:'',
 			stack:[],
-			registers:{R:0,I:0,P:0,L:0,Z:0}
+			registers:{R:0,I:0,P:0,L:0,Z:0},
 			vars:{},
 			output:''
 		},
@@ -34,18 +34,18 @@
 					return:'',
 					input:input,
 					stack:[],
-					registers:{R:0,I:0,P:0,L:0,Z:0}
+					registers:{R:0,I:0,P:0,L:0,Z:0},
 					vars:{},
 					output:''
 				};
 				
-				return runBlock( memory.code );
+				return memory.return = runBlock( memory.code );
 			},
 			getStack: function(){ return memory.stack; },
 			getReturn: function(){ return memory.return; },
 			getVars: function(){ return memory.vars; },
 			getOutput: function(){ return memory.output; }
-		}
+		};
 		
 	};
 	
